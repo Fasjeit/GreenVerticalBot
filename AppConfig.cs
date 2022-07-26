0807dc6a856a641802bb68f13af219bfa6b14e38
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2022-07-26 11:48:02 +0300

    Initial commit

---
+++ AppConfig.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace GvBot
+{
+    /// <summary>
+    /// Конфигурация бота
+    /// </summary>
+    internal class AppConfig
+    {
+        /// <summary>
+        /// Токен бота (получаем через BotFather телеграма)
+        /// </summary>
+        public string BotToken { get; set; }
+
+        /// <summary>
+        /// Идентификатор закрытого чата, в который бот будет присылать инвайты. 
+        /// Бот должен иметь права адмна в чате
+        /// </summary>
+        public long PrivateChatId { get; set; }
+
+        private AppConfig()
+        {
+        }
+
+        /// <summary>
+        /// Зачитывает конфиг из локального json файла
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<AppConfig> GetConfigAsync()
+        {
+            return JsonConvert.DeserializeObject<AppConfig>(await File.ReadAllTextAsync(@"config.json"));
+        }
+    }
+}
